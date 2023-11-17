4cecf2851700e840df651ecc8927c729363c319b
viennguyenbkdn <91453629+viennguyenbkdn@users.noreply.github.com>
2023-11-17 11:52:43 +0700

    Update avail_monitoring_setup.sh

--- avail_monitoring_setup.sh
+++ avail_monitoring_setup.sh
@@ -15,15 +15,15 @@ sleep 1;
 HOST_IP=$(curl -s ipinfo.io/ip);
 sed -i.bak -e "s/^HOST_IP=.*/HOST_IP=$HOST_IP/g" $HOME/Avail_Monitoring/.env
 sed -i.bak -e "s/HOST_IP/$HOST_IP/g" $HOME/Avail_Monitoring/prometheus/prometheus.yml
-
-docker-compose -f docker-compose.yml up -d;
-sleep 1;
-docker-compose down;
-sleep 1;
-
 chmod -R 777 $HOME/Avail_Monitoring 
-docker-compose -f docker-compose.yml up -d;
-sleep 3;
+docker compose up -d;
+sleep 1;
+# docker compose down;
+# sleep 1;
+
+
+# docker compose up -d;
+# sleep 3;
 
 PORT_GRAFANA=$(sed -n 's/^PORT_GRAFANA=\(.*\)/\1/p' $HOME/Avail_Monitoring/.env) 
 
