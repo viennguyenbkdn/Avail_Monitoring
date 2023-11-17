7c009238345db30538753d7655bd7d37b2f3d430
viennguyenbkdn <91453629+viennguyenbkdn@users.noreply.github.com>
2023-11-17 11:27:24 +0700

    Add files via upload

---
+++ avail_monitoring_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -e "
+\e[42mBriefly introduction:\e[0m
+- The script is used to setup monitoring tool of your Avail Validator node.
+- All collected metrics will be output Prometheus and Grafana
+"
+
+
+cd $HOME
+git clone https://viennguyenbkdn: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@github.com/viennguyenbkdn/Avail_Monitoring
+
+HOST_IP=$(curl -s ipinfo.io/ip);
+
+sed -i.bak -e "s/^HOST_IP=.*/HOST_IP=$HOST_IP/g" $HOME/Avail_Monitoring/.env
+sed -i.bak -e "s/HOST_IP/$HOST_IP/g" $HOME/Avail_Monitoring/prometheus/prometheus.yml
+
+
+chmod -R 777 $HOME/Avail_Monitoring 
+cd $HOME/Avail_Monitoring 
+
+docker-compose -f docker-compose.yml up -d
+
+
+PORT_GRAFANA=$(sed -n 's/^PORT_GRAFANA=\(.*\)/\1/p' $HOME/Avail_Monitoring/.env) 
+
+
+echo -e "\n\033[0;32mYou have finished installation of monitoring DA node\033[0m"
+echo -e "\nLogin Grafana link: \033[0;31mhttp://$HOST_IP:$PORT_GRAFANA\033[0m"
